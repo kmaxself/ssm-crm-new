ce7fb571ecf5701ef3b3f1ab66f022dadaae480e
kmaxself <guokai1080@126.com>
2017-03-28 22:35:34 +0800

    dep merge

--- test/ssm/Demo.java
+++ test/ssm/Demo.java
@@ -18,6 +18,7 @@ public class Demo {
  /**
   * 读取一张图片的RGB值
   * @throws Exception
+  * the master branch
   */
  public void getImagePixel(String image) throws Exception {
   int[] rgb = new int[3];
